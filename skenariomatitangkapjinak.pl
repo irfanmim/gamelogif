33480d29e99ae952ad914365ea97754b470a40ae
Faiz Haznitrama <Faiz Haznitrama>
2016-11-29 17:26:43 +0700

    skenario musuh kalah

--- skenariomatitangkapjinak.pl
+++ skenariomatitangkapjinak.pl
@@ -1,22 +1,37 @@
 /* skenario kalau tertangkap/mati/jinak */
 
 /*COPET*/
+	/*TERTANGKAP*/
 	write('HAP! Komplotan copet sudah tertangkap dengan baik. Borgol itu kuat,'), nl,
-	write('mereka tidak akan bisa kabur. Bawa mereka ke Bos Kapolri laporan dan'), nl,
+	write('mereka tidak akan bisa kabur. Bawa mereka ke Bos Kapolri untuk laporan dan'), nl,
 	write('penindakan!'), nl.
+	/*MATI*/
+	write('DORR! Tembakan mu terlalu kuat! Peluru mu melesat menuju dada para copet!'), nl,
+	write('Yah bagaimanapun, kau sudah berhasil memberantas para komplotan copet, meskipun'), nl,
+	write('sebenarnya caramu terlalu keras. Tapi tak apa, laporkan keberhasilan mu pada bos!'), nl.
 
 /*TERORIS*/
+	/*TERTANGKAP*/
 	write('Satu lagi oknum kejahatan yang meresahkan telah tertangkap. Pemikiran'), nl,
 	write('mereka yang seperti psikopat dan radikal memang perlu ditindak. Kerja bagus,'), nl,
 	write('segera beritahu Bos Kapolri!'), nl.
+	/*MATI*/
+	write('CRASSS!! Darah bercucuran dari para teroris! Senjata yang kau gunakan terlalu'), nl,
+	write('berlebihan! Kau membunuh mereka semua! Ini bukan hal yang cukup baik, tetapi setidaknya'), nl,
+	write('kejahatan akan berkurang sekarang. Laporkan misimu pada bos.'), nl.
 
 /*MAFIA*/
+	/*TERTANGKAP*/
 	write('Raja kejahatan kota sudah berhasil kau tangkap! Luar biasa! Sekarang dia tidak'), nl,
 	write('akan bisa menebar anak-buahnya untuk menebar ketakutan di kota. Kau memang hebat!'), nl,
 	write('Segera kembali ke Bos!'), nl.
+	/*MATI*/
+	write('Sebuah tindakan berlebihan. Kau justru membantai mereka! Bazooka mu memiliki'), nl,
+	write('ledakan yang sangat kuat! Hmm, segera laporkan pada bos.'), nl.
+
 
 /*BERUANG*/
-	write('WOOOOO! Beruang itu sekarang sudah tenang, sepertinya dia menjadi mengantuk dan tidur.'), nl,
+	write('WOOooo! Beruang itu sekarang sudah tenang, sepertinya dia menjadi mengantuk dan tidur.'), nl,
 	write('Kerja bagus, orang-orang di kebun binatang sudah mulai kembali tenang juga. Kembalilah'), nl,
 	write('pada zookeeper dan Bos!'), nl.
 
