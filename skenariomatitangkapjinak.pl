ebe2fe888a536e1c3e2e59a22ed959eaf607b946
Faiz Haznitrama <Faiz Haznitrama>
2016-11-29 08:43:18 +0700

    skenario ketika berhasil mengalahkan musuh

---
+++ skenariomatitangkapjinak.pl
@@ -0,0 +1,26 @@
+/* skenario kalau tertangkap/mati/jinak */
+
+/*COPET*/
+	write('HAP! Komplotan copet sudah tertangkap dengan baik. Borgol itu kuat,'), nl,
+	write('mereka tidak akan bisa kabur. Bawa mereka ke Bos Kapolri laporan dan'), nl,
+	write('penindakan!'), nl.
+
+/*TERORIS*/
+	write('Satu lagi oknum kejahatan yang meresahkan telah tertangkap. Pemikiran'), nl,
+	write('mereka yang seperti psikopat dan radikal memang perlu ditindak. Kerja bagus,'), nl,
+	write('segera beritahu Bos Kapolri!'), nl.
+
+/*MAFIA*/
+	write('Raja kejahatan kota sudah berhasil kau tangkap! Luar biasa! Sekarang dia tidak'), nl,
+	write('akan bisa menebar anak-buahnya untuk menebar ketakutan di kota. Kau memang hebat!'), nl,
+	write('Segera kembali ke Bos!'), nl.
+
+/*BERUANG*/
+	write('WOOOOO! Beruang itu sekarang sudah tenang, sepertinya dia menjadi mengantuk dan tidur.'), nl,
+	write('Kerja bagus, orang-orang di kebun binatang sudah mulai kembali tenang juga. Kembalilah'), nl,
+	write('pada zookeeper dan Bos!'), nl.
+
+/*ZOMBIE_HITLER*/
+	write('GROoooooaaaa....! Hitler tidak kuat melawan ledakan dari bazooka mu, dia pun terjatuh.'), nl,
+	write('Sepertinya ia telah kembali ke kematian untuk sekali lagi. Sungguh mengerikan. Semoga'), nl,
+	write('ini terakhir kalinya ia kembali hidup. Laporkan kepada bos!'), nl.
